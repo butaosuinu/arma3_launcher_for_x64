5caa116e4847ff24edd01e6f36989c41b3a74f34
butaosuinu <buta.osuinu@gmail.com>
2017-04-19 21:41:26 +0900

    Change build process

--- webpack.config.js
+++ webpack.config.js
@@ -8,74 +8,73 @@ var NodeTargetPlugin = require('webpack/lib/node/NodeTargetPlugin');
 var ExternalsPlugin = webpack.ExternalsPlugin;
 
 module.exports = {
-	entry: {
-		main: './src/js/render.js',
-	},
-	output: {
-		path: __dirname + '/app/render',
-		filename: '[name].js'
-	},
-	plugins: [
-		new webpack.ProvidePlugin({ riot: 'riot' }),
-		new webpack.NoErrorsPlugin(),
-		new ExternalsPlugin('commonjs', [
-			'electron',
-			'app',
-			'auto-updater',
-			'browser-window',
-			'content-tracing',
-			'dialog',
-			'global-shortcut',
-			'ipc',
-			'menu',
-			'menu-item',
-			'power-monitor',
-			'protocol',
-			'tray',
-			'remote',
-			'web-frame',
-			'clipboard',
-			'crash-reporter',
-			'screen',
-			'shell'
-		]),
-		new NodeTargetPlugin()
-		// commonsPlugin
-		// new webpack.optimize.UglifyJsPlugin()
-	],
-	module: {
-		rules: [
-			{
-				test: /\.tag$/,
-				enforce: "pre",
-				exclude: /node_modules/,
-				use: {
-					loader: 'riotjs-loader',
-					query: {
-						type: ['babel', 'scss']
-					}
-				}
-			},
-			{
-				test: /\.js$|\.tag$/,
-				exclude: /node_modules/,
-				use: 'babel-loader',
-				use: {
-					loader: 'babel-loader',
-					query: {
-						presets: ['es2015']
-					}
-				}
-			}
-		]
-	},
-	resolve: {
-		modules: ['node_modules', 'bower_components'],
-		descriptionFiles: ['package.json', 'bower.json'],
-		extensions: ['.js', '.tag'],
-		alias: {
-			uikit: 'uikit/js/uikit.js',
-			components: 'bower_components/uikit/js/components'
-		}
-	}
+  entry: {
+    main: './src/js/render.js',
+  },
+  output: {
+    path: __dirname + '/app/render',
+    filename: '[name].js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({ riot: 'riot' }),
+    new webpack.NoErrorsPlugin(),
+    new ExternalsPlugin('commonjs', [
+      'electron',
+      'app',
+      'auto-updater',
+      'browser-window',
+      'content-tracing',
+      'dialog',
+      'global-shortcut',
+      'ipc',
+      'menu',
+      'menu-item',
+      'power-monitor',
+      'protocol',
+      'tray',
+      'remote',
+      'web-frame',
+      'clipboard',
+      'crash-reporter',
+      'screen',
+      'shell'
+    ]),
+    new NodeTargetPlugin()
+    // commonsPlugin
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tag$/,
+        enforce: "pre",
+        exclude: /node_modules/,
+        use: {
+          loader: 'riotjs-loader',
+          query: {
+            type: ['babel', 'scss']
+          }
+        }
+      },
+      {
+        test: /\.js$|\.tag$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['latest']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    modules: ['node_modules', 'bower_components'],
+    descriptionFiles: ['package.json', 'bower.json'],
+    extensions: ['.js', '.tag'],
+    alias: {
+      uikit: 'uikit/js/uikit.js',
+      components: 'bower_components/uikit/js/components'
+    }
+  }
 };
