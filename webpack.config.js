5124e4bc19a58b28721ea75576c5dce74bd5284e
butaosuinu <buta.osuinu@gmail.com>
2017-04-19 23:27:46 +0900

    Change css build process with webpack #6

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,9 @@ var JsonpTemplatePlugin = webpack.JsonpTemplatePlugin;
 var FunctionModulePlugin = require('webpack/lib/FunctionModulePlugin');
 var NodeTargetPlugin = require('webpack/lib/node/NodeTargetPlugin');
 var ExternalsPlugin = webpack.ExternalsPlugin;
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
+module.exports = [{
   entry: {
     main: './src/js/render.js',
   },
@@ -77,4 +78,30 @@ module.exports = {
       components: 'bower_components/uikit/js/components'
     }
   }
-};
+}, {
+  entry: {
+    main: './src/sass/main.scss'
+  },
+  output: {
+    path: path.join(__dirname + '/app/render/css'),
+    filename: '[name].css'
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css'),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader!sass-loader"
+        })
+      },
+      {
+        test: /\.(otf|eot|svg|ttf|woff|woff2)(\?.+)?$/,
+        use: 'url-loader'
+      }
+    ]
+  }
+}];
